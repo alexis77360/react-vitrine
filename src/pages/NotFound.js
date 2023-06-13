9c25502898c400b20995f32481ba2ef88e5f30a6
alexis77360 <84974466+alexis77360@users.noreply.github.com>
2023-06-13 22:59:29 +0200

    mise en place sass + routes

---
+++ src/pages/NotFound.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div> 
+            <div className="notFound">
+                <div className="notFound-content">
+                    <h2>Erreur 404</h2>
+                </div>
+
+            </div>
+            
+        </div>
+    );
+};
+
+export default NotFound;
---
+++ src/pages/Project2.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Project2 = () => {
+    return (
+        <div>
+            <h1>Project2</h1>
+            
+        </div>
+    );
+};
+
+export default Project2;
